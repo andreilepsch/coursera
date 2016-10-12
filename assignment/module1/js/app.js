2716d283e1f386ac678e21d9101ada1e7fd1f79f
Andrei Lepsch <andrei.lepsch@gmail.com>
2016-10-11 23:53:23 -0300

    Finish the Assignment Module 1

--- assignment/module1/js/app.js
+++ assignment/module1/js/app.js
@@ -1,11 +1,36 @@
 (function () {
+'use strict';
 
 angular.module("LunchCheck",[])
     .controller("LunchCheckController", LunchCheckController);
 
 LunchCheckController.$inject = ["$scope", "$filter"];
 function LunchCheckController($scope, $filter){
-  $scope.name = "teste";
+
+  $scope.lunch = "";
+  $scope.LunchCheck = function () {
+    var countItens = 0;
+    var myLunch = $scope.lunch.split(",");
+
+    for(var i = 0; i < myLunch.length; i++) {
+      if(myLunch[i] !== "") {
+        countItens += 1;
+      }
+    }
+    if ($scope.lunch === "") {
+      $scope.message = "Please enter data first";
+      $scope.color = "red";
+      $scope.borderColor = "borderRed";
+    } else if (countItens <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.color = "green";
+      $scope.borderColor = "borderGreen";
+
+    } else if (countItens > 3) {
+      $scope.message = "Too much!";
+    }
+  };
+
 }
 
 })();
