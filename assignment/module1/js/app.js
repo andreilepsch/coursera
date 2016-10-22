ada3bbd5e2a9a4c759fadad056e3097c7b255d4a
Andrei Lepsch <andrei.lepsch@gmail.com>
2016-10-22 21:32:44 -0200

    Add assignment 1 to new directory

---
+++ assignment/module1/js/app.js
@@ -0,0 +1,37 @@
+(function () {
+'use strict';
+
+angular.module("LunchCheck",[])
+    .controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$inject = ["$scope", "$filter"];
+function LunchCheckController($scope, $filter){
+
+  $scope.lunch = "";
+  $scope.LunchCheck = function () {
+    var countItens = 0;
+    var myLunch = $scope.lunch.split(",");
+    console.log(myLunch);
+    for(var i = 0; i < myLunch.length; i++) {
+      if(myLunch[i].trim() !== "") {
+        console.log(myLunch[i].trim());
+        countItens += 1;
+      }
+    }
+    if ($scope.lunch === "") {
+      $scope.message = "Please enter data first";
+      $scope.color = "red";
+      $scope.borderColor = "borderRed";
+    } else if (countItens <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.color = "green";
+      $scope.borderColor = "borderGreen";
+
+    } else if (countItens > 3) {
+      $scope.message = "Too much!";
+    }
+  };
+
+}
+
+})();
