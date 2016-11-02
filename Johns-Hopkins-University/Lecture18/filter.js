1a9b24d4d691dc358a2b830577cdcd8e6c851122
Andrei Lepsch <andrei.lepsch@gmail.com>
2016-11-02 19:58:29 -0200

    Add Lecture19 and add some code in filter.js in Lecture18

--- Johns-Hopkins-University/Lecture18/filter.js
+++ Johns-Hopkins-University/Lecture18/filter.js
@@ -1,2 +1,16 @@
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Number array: ", numberArray);
+
+var above5Filter = function (value) {
+  return value > 5;
+}
+
+var filterNumberArray = numberArray.filter(above5Filter);
+console.log("Filtered Number: ", filterNumberArray);
+
+var shoppingList = [
+  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
+console.log("Shopping List:", shoppingList);
+
+var searchValue = "Bismol";
