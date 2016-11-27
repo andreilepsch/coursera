96c306783b21b46a32aa1ef515db9969344b1313
Andrei Lepsch <andrei.lepsch@gmail.com>
2016-11-27 19:29:36 -0200

    Add angular-route and set routes in app.js

--- Hong-Kong-University/front-end-angular-js/conFusion/app/scripts/app.js
+++ Hong-Kong-University/front-end-angular-js/conFusion/app/scripts/app.js
@@ -1,3 +1,30 @@
 'use strict';
 
-angular.module('confusionApp', ["ngRoute"]);
+angular.module('confusionApp', ["ngRoute"])
+
+  .config(function($routeProvider) {
+    $routeProvider
+
+      //Route to contactus page
+      .when('/contactus',{
+        templateUrl: 'contactus.html',
+        controller: 'ContactController'
+      })
+
+      //Route to menu page
+      .when('/menu',{
+        templateUrl: 'menu.html',
+        controller: 'MenuController'
+      })
+
+      //Route to dish details page
+      .when('/menu/:id',{
+        templateUrl: 'dishdetail.html',
+        controller: 'DishDetailController'
+      })
+
+      .otherwise('/contactus');
+
+
+
+  });
